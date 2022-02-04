51ca1ef257a724ac1060ae560c7102b15c1bca91
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-02-04 15:02:33 -0800

    Installed react router and setup base components and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/*' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   )
 }
