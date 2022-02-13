93398b366215bba46c58365cec780af2b5115bef
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-02-13 02:12:08 -0800

    Added hover effect to the links on the navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,19 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className='header-content'>
+        <h1>
+          I am Nolan The{' '}
+          <span
+            className='txt-type'
+            data-wait='3000'
+            data-words='["Designer", "UI/UX Dev"]'></span>
+          <p className='lead'>I specialize in Graphic Design UI/UX</p>
+          <a href='' className='lead'>
+            View My Work
+          </a>
+        </h1>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,60 +8,47 @@ const Navbar = () => {
       <header id='header-home'>
         <div className='container'>
           <nav id='main-nav'>
-            <img
-              src='../../../../../../img/logo.png'
-              alt='My Portfolio'
-              id='logo'
-            />
+            <img src='' alt='My Portfolio' id='logo' />
             <ul>
               <li>
                 <a
                   onclick={() => {
-                    navigate.push('/')
+                    navigate('/')
                   }}
                   href='*'
-                  className='current'></a>
-                Home
+                  className='current'>
+                  Home
+                </a>
               </li>
               <li>
                 <a
                   onClick={() => {
                     navigate('/about')
                   }}
-                  href='*'></a>
-                About
+                  href='*'>
+                  About
+                </a>
               </li>
               <li>
                 <a
                   onClick={() => {
                     navigate('/work')
                   }}
-                  href='*'></a>
-                Work
+                  href='*'>
+                  Work
+                </a>
               </li>
               <li>
                 <a
                   onClick={() => {
                     navigate('/contact')
                   }}
-                  href='*'></a>
-                Contact
+                  href='*'>
+                  Contact
+                </a>
               </li>
             </ul>
           </nav>
-          <div className='header-content'>
-            <h1>
-              I am Nolan The{' '}
-              <span
-                className='txt-type'
-                data-wait='3000'
-                data-words='["Designer", "UI/UX Dev"]'></span>
-              <p className='lead'>I specialize in Graphic Design UI/UX</p>
-              <a href='' className='lead'>
-                View My Work
-              </a>
-            </h1>
-          </div>
         </div>
       </header>
     </div>
