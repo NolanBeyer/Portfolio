0496c95a34332806d4ccd76d1be9970d61cc6dc5
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-02-14 00:44:33 -0800

    Finised home page and fixed links on nav / footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,20 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div className='header-content'>
-        <h1>
-          I am Nolan The{' '}
-          <span
-            className='txt-type'
-            data-wait='3000'
-            data-words='["Designer", "UI/UX Dev"]'></span>
+      <div className='container'>
+        <div className='header-content'>
+          <h1>
+            I am Nolan The{' '}
+            <span
+              className='txt-type'
+              data-wait='3000'
+              data-words='["Designer", "UI/UX Dev"]'></span>
+          </h1>
           <p className='lead'>I specialize in Graphic Design UI/UX</p>
-          <a href='' className='lead'>
+          <a href='' className='btn-light'>
             View My Work
           </a>
-        </h1>
+        </div>
       </div>
     </div>
   )
