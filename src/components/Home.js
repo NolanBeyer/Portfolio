5677e86ae242fc6292537d6d8451e3b01e056df7
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-02-16 00:57:52 -0800

    Finished specailze & stats section styling/markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div className='container'>
-        <div className='header-content'>
+      <div className='home-content'>
+        <div className='container'>
           <h1>
             I am Nolan The{' '}
             <span
@@ -18,6 +18,103 @@ const Home = () => {
           </a>
         </div>
       </div>
+
+      {/* Section-A Specialize */}
+      <section id='home-a' className='text-center py-2'>
+        <div className='container'>
+          <h2 className='section-title'>I Specialize In</h2>
+          <div className='bottom-line'></div>
+          <p className='lead'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab magnam,
+            amet eius recusandae cum ducimus accusantium a neque similique
+            temporibus?
+          </p>
+          <div className='specials'>
+            <div className='py-2'>
+              <i className='fas fa-file-alt fa-2x'></i>
+              <h3>Concepting</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium possimus, quaerat molestiae suscipit doloribus sint
+                enim sapiente vero facere quibusdam? Ea quaerat molestias ullam
+                sint ratione, beatae id mollitia ducimus!
+              </p>
+            </div>
+            <div className='py-2'>
+              <i className='fas fa-desktop fa-2x'></i>
+              <h3>UI / UX</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium possimus, quaerat molestiae suscipit doloribus sint
+                enim sapiente vero facere quibusdam? Ea quaerat molestias ullam
+                sint ratione, beatae id mollitia ducimus!
+              </p>
+            </div>
+            <div className='py-2'>
+              <i className='fas fa-object-ungroup fa-2x'></i>
+              <h3>Visual Design</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium possimus, quaerat molestiae suscipit doloribus sint
+                enim sapiente vero facere quibusdam? Ea quaerat molestias ullam
+                sint ratione, beatae id mollitia ducimus!
+              </p>
+            </div>
+            <div className='py-2'>
+              <i className='fas fa-thumbs-up fa-2x'></i>
+              <h3>Visual Design</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium possimus, quaerat molestiae suscipit doloribus sint
+                enim sapiente vero facere quibusdam? Ea quaerat molestias ullam
+                sint ratione, beatae id mollitia ducimus!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Section B: Stats */}
+      <section id='home-b' className='text-center'>
+        <div className='stats'>
+          <div className='odds'>
+            <ul>
+              <li>
+                <i className='fas fa-users fa-3x'></i>
+              </li>
+              <li className='stats-title'>Clients</li>
+              <li className='stats-number'>100</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <i className='fas fa-award fa-3x'></i>
+              </li>
+              <li className='stats-title'>Awards</li>
+              <li className='stats-number'>3</li>
+            </ul>
+          </div>
+          <div className='odds'>
+            <ul>
+              <li>
+                <i className='fas fa-hourglass-start fa-3x'></i>
+              </li>
+              <li className='stats-title'>Hours Worked</li>
+              <li className='stats-number'>3500</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <i className='fas fa-code-branch fa-3x'></i>
+              </li>
+              <li className='stats-title'>Projects</li>
+              <li className='stats-number'>135</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
