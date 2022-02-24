836da5667b648542491d2e2cf041abea2336c26d
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-02-23 18:48:19 -0800

    styled footer / added position property to stay 0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,22 @@ const Footer = () => {
         <div className='footer-content container'>
           <p>Copyright &copy; 2022. All rights reserved.</p>
           <div className='social'>
-            <a
-              href='https://github.com/NolanBeyer'
-              className='fab fa-github'></a>
+            <a href='https://github.com/NolanBeyer' className='fab fa-github'>
+              {' '}
+            </a>
             <a
               href='https://www.linkedin.com/in/nolan-beyer-5252731b1/'
-              className='fab fa-linkedin'></a>
-            <a href='https://www.facebook.com/' className='fab fa-facebook'></a>
+              className='fab fa-linkedin'>
+              {' '}
+            </a>
+            <a href='https://www.facebook.com/' className='fab fa-facebook'>
+              {' '}
+            </a>
             <a
               href='https://www.instagram.com/nb127_/'
-              className='fab fa-instagram'></a>
+              className='fab fa-instagram'>
+              {' '}
+            </a>
           </div>
         </div>
       </footer>
