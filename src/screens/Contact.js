84e847eea1fb33f8ab3ddecf14bfe113be7554e0
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-03-03 14:32:14 -0800

    Added git ignore file

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -32,31 +32,39 @@ const Contact = () => {
         <div className='container'>
           <h2 className='section-title'>Contact Me</h2>
           <div className='bottom-line'></div>
-          <form action=''>
+          <form method='POST' data-netlify='true' data-netlify-recaptcha='true'>
             <div className='text-fields'>
               <input
                 type='text'
                 className='text-input name-input'
                 placeholder='Name'
+                name='name'
               />
               <input
                 type='text'
                 className='text-input subject-input'
                 placeholder='Subject'
+                name='subject'
               />
               <input
                 type='email'
                 className='text-input email-input'
                 placeholder='Email Address'
+                name='email'
               />
               <input
                 type='text'
                 className='text-input phone-input'
                 placeholder='Phone Number'
+                name='phone'
               />
               <textarea
                 className='text-input message-input'
-                placeholder='Enter Message'></textarea>
+                placeholder='Enter Message'
+                name='message'></textarea>
+              <div className='my-2'>
+                <div data-netlify-recaptcha='true'></div>
+              </div>
             </div>
             <button type='submit' className='btn-dark'>
               Submit
