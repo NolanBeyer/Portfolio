449b714385812a68dfc133fd02e27231cd9fdd23
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-03-05 14:16:49 -0800

    Added new section to about page and fixed styling

--- src/screens/About.js
+++ src/screens/About.js
@@ -34,17 +34,40 @@ const About = () => {
             <div>
               <img className='about-img' src='/img/me.jpg' alt='Me' />
             </div>
-            <div className='bio bg-light py-4'>
+            <div className='bio bg-medium py-4'>
               <h4>Bio</h4>
               <p>
                 I've always been really interested in technology, but never
                 thought about a career in web development until after I suffered
-                a SCI back in 2016. I started doing some research and just
-                trying to learn some of the basics in HTML and CSS in late 2019.
-                I've spent countless hours over the last two years learning /
-                teaching myself. Although I still have a ways to go I'm really
-                excited for this next step and am willing to learn additional
+                a SCI back in 2016. In 2019 I started doing some research and
+                just trying to learn some of the basics of HTML and CSS. I've
+                spent countless hours over the last two years learning /
+                teaching myself these languages. I feel like now I am more than
+                ready for a frontend position and am willing to learn additional
                 technologies to fit the needs of future employers.
+              </p>
+            </div>
+            <div className='award-1 py-4'>
+              <i className='fas fa-user-graduate fa-3x'></i>
+              <h3>DevMtn Graduate</h3>
+              <p>
+                Full-Stack Web Development Fully Remote Immersive Coding Course
+              </p>
+            </div>
+            <div className='award-2 py-4'>
+              <i class='fas fa-certificate fa-3x'></i>
+              <h3>MERN Ecommnerce</h3>
+              <p>
+                Full stack Ecommerce site with authentication. Built with
+                MongoDB, Express, React, and Node.
+              </p>
+            </div>
+            <div className='award-3 py-4'>
+              <i class='fas fa-certificate fa-3x'></i>
+              <h3>Web Developer Bootcamp</h3>
+              <p>
+                Full stack online bootcamp. Learning HTML, CSS, JavaScript,
+                express, node, etc...
               </p>
             </div>
           </div>
